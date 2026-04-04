991cc3e94417340efdcf0064aef932bcc317b33d
Haoxin Xia <haoxinxi@usc.edu>
2026-04-04 13:35:22 +0800

    jimmy's change: make agents functional
    I added systems prompts to all the three agents. Making it works
    properly.
    
    Signed-off-by: Haoxin Xia <haoxinxi@usc.edu>

--- src/product_strategy_generator/agents/idea_analyst.py
+++ src/product_strategy_generator/agents/idea_analyst.py
@@ -8,9 +8,11 @@ class IdeaAnalystAgent(BaseAgent):
     def system_prompt(self) -> str:
         return (
             """
+            Just tell briefly, from high level how user's idea can be implemnted. Your output should look like a prompt which can be given to another agent which can read it and work towards producing a strategy. It should be small like one paragraph.
             """
         )
 
     def build_user_prompt(self, *, idea: str) -> str:
-        return f"""
-                """.strip()
+        #return f"""
+        #        """.strip()
+        return idea
